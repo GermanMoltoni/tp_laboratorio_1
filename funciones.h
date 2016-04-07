e557210d94eef7a53b41c1219858815774be6dc6
GermanMoltoni <german.moltoni@gmail.com>
2016-04-07 20:55:43 -0300

    Trabajo Practico N°1
    Calculadora

---
+++ funciones.h
@@ -0,0 +1,13 @@
+
+
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+float sumar(float ,float );
+float restar(float ,float );
+float dividir(float ,float );
+float multiplicar(float ,float );
+long long int factorial(long long int );
+unsigned int validarIngreso(unsigned int,unsigned int);
+unsigned int validarFactorial(float, unsigned int);
+
+#endif // FUNCIONES_H_INCLUDED
