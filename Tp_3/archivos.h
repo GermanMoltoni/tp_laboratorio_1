43c3d594d4246cf209368053e544f7f28b962d54
GermanMoltoni <german.moltoni@gmail.com>
2016-05-27 06:14:29 -0300

    Tp_3
    Trabajo Practico N°3 Archivos

--- Tp_3/archivos.h
+++ Tp_3/archivos.h
@@ -6,5 +6,5 @@ int cargarDesdeArchivo(char* name,EMovie* movies,int length);
 int cargarHaciaArchivo(char* name,EMovie* movies,int length);
 int leerBin(char* nombre,EMovie* movies,int length,FILE* pArch);
 int crearArchivoWeb(char* name,EMovie* movies,int length);
-int fileCopy(FILE* pAux,FILE* pArch)
+int fileCopy(FILE* pAux,FILE* pArch);
 #endif // FUNCIONES_H_INCLUDED
